bd0dd1b428b5ebf3d8768023fb65b081a643073c
Leshchik_Vadim <vleshchik@gmail.com>
2023-08-27 17:59:32 +0300

    Initial commit

---
+++ src/main/java/Aston/lesson2/printColor.java
@@ -0,0 +1,15 @@
+package Aston.lesson2;
+
+public class printColor {
+    public static void main(String[] args) {
+        int num = 100;
+
+        if (num > 100) {
+            System.out.println("Зеленый");
+        }else if (num <= 0) {
+            System.out.println("Красный");
+        } else {
+            System.out.println("Желтый");
+        }
+    }
+}
---
+++ src/main/java/Aston/lesson2/printThreeWords.java
@@ -0,0 +1,9 @@
+package Aston.lesson2;
+
+public class printThreeWords {
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
