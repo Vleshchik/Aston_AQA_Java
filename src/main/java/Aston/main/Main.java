8c672e3c01d97cd663660e2f6902cb0329e4a657
Leshchik_Vadim <vleshchik@gmail.com>
2023-09-17 13:18:35 +0300

    Lesson 7 answer

--- src/main/java/Aston/main/Main.java
+++ src/main/java/Aston/main/Main.java
@@ -1,4 +1,39 @@
 package Aston.main;
 
+
 public class Main {
+    static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        // Проверка размера массива
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Неверный размер массива");
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Неверный размер массива");
+            }
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]", e);
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        String[][] array = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
+
+        try {
+            int sum = sumArray(array);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в массиве: " + e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/Aston/main/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package Aston.main;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
