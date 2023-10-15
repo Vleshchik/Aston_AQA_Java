be39db4b7e7acc5b36db796924895a8b703aec9a
Leshchik_Vadim <vleshchik@gmail.com>
2023-10-15 14:27:46 +0300

    Lesson 14 (v2.0)

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -15,42 +15,43 @@ public class Main {
         WebDriverWait wait = new WebDriverWait(driver,5);
         driver.get("https://www.mts.by/");
         wait.until(ExpectedConditions.titleContains("МТС – мобильный оператор в Беларуси"));
-        WebElement continueButton = driver.findElement(By.xpath("//button[@class='cookie__close']"));
-        scrollToCenter(driver, continueButton, actions);
-        continueButton.click();
-        //Надписи в поле Услуги связи //input[contains(@placeholder,'Номер телефона')]
-        WebElement blockTitle = driver.findElement(By.id("connection-phone"));
-        scrollToCenter(driver, blockTitle, actions);
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle.getText());
-        WebElement blockTitle_2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("///input[@id='connection-sum']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_2.getText());
-        WebElement blockTitle_3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='E-mail для отправки чека']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_3.getText());
+        WebElement cookieButton = driver.findElement(By.xpath("//button[@class='cookie__close']"));
+        scrollToCenter(driver, cookieButton, actions);
+        cookieButton.click();
+        //Надписи в поле Услуги связи
+        WebElement blockTitlePhone = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Номер телефона']")));
+        scrollToCenter(driver, blockTitlePhone, actions);
+        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitlePhone.getAttribute("placeholder"));
+        WebElement blockTitlePhone_2 = driver.findElement(By.xpath("//input[@id='connection-sum']"));
+        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitlePhone_2.getAttribute("placeholder"));
+        WebElement blockTitlePhone_3 = driver.findElement(By.xpath("//input[@placeholder='E-mail для отправки чека']"));
+        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitlePhone_3.getAttribute("placeholder"));
+        System.out.println("___________________________________________________________________");
         //Надписи в поле Домашний интернет
-        WebElement blockTitle = driver.findElement(By.id("connection-phone"));
-        scrollToCenter(driver, blockTitle, actions);
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle.getText());
-        WebElement blockTitle_2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("///input[@id='connection-sum']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_2.getText());
-        WebElement blockTitle_3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='E-mail для отправки чека']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_3.getText());
+        WebElement blockTitleHomeInternet_1 = driver.findElement(By.xpath("//input[@id='internet-phone']"));
+        System.out.println("Надпись в незаполненном поле Домашний интернет: " + blockTitleHomeInternet_1.getAttribute("placeholder"));
+        WebElement blockTitleHomeInternet_2 = driver.findElement(By.xpath("//input[@id='internet-sum']"));
+        System.out.println("Надпись в незаполненном поле Домашний интернет: " + blockTitleHomeInternet_2.getAttribute("placeholder"));
+        WebElement blockTitleHomeInternet_3 = driver.findElement(By.xpath("//input[@id='internet-email']"));
+        System.out.println("Надпись в незаполненном поле Домашний интернет: " + blockTitleHomeInternet_3.getAttribute("placeholder"));
+        System.out.println("___________________________________________________________________");
         //Надписи в поле Рассрочка
-        WebElement blockTitle = driver.findElement(By.id("connection-phone"));
-        scrollToCenter(driver, blockTitle, actions);
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle.getText());
-        WebElement blockTitle_2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("///input[@id='connection-sum']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_2.getText());
-        WebElement blockTitle_3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='E-mail для отправки чека']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_3.getText());
+        WebElement blockTitleInstalment_1 = driver.findElement(By.xpath("//input[@id='score-instalment']"));
+        System.out.println("Надпись в незаполненном поле Рассрочка: " + blockTitleInstalment_1.getAttribute("placeholder"));
+        WebElement blockTitleInstalment_2 = driver.findElement(By.xpath("//input[@id='instalment-sum']"));
+        System.out.println("Надпись в незаполненном поле Рассрочка: " + blockTitleInstalment_2.getAttribute("placeholder"));
+        WebElement blockTitleInstalment_3 = driver.findElement(By.xpath("//input[@id='instalment-email']"));
+        System.out.println("Надпись в незаполненном поле Рассрочка: " + blockTitleInstalment_3.getAttribute("placeholder"));
+        System.out.println("___________________________________________________________________");
         //Надписи в поле Задолженность
-        WebElement blockTitle = driver.findElement(By.id("connection-phone"));
-        scrollToCenter(driver, blockTitle, actions);
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle.getText());
-        WebElement blockTitle_2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("///input[@id='connection-sum']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_2.getText());
-        WebElement blockTitle_3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='E-mail для отправки чека']")));
-        System.out.println("Надпись в незаполненном поле Услуги связи: " + blockTitle_3.getText());
-/*
+        WebElement blockTitleArrears_1 = driver.findElement(By.xpath("//input[@id='score-arrears']"));
+        System.out.println("Надпись в незаполненном поле Задолженность: " + blockTitleArrears_1.getAttribute("placeholder"));
+        WebElement blockTitleArrears_2 = driver.findElement(By.xpath("//input[@id='arrears-sum']"));
+        System.out.println("Надпись в незаполненном поле Задолженность: " + blockTitleArrears_2.getAttribute("placeholder"));
+        WebElement blockTitleArrears_3 = driver.findElement(By.xpath("//input[@id='arrears-email']"));
+        System.out.println("Надпись в незаполненном поле Задолженность: " + blockTitleArrears_3.getAttribute("placeholder"));
+        System.out.println("___________________________________________________________________");
+
         //задание 2
         WebElement serviceRadio = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='pay__form']")));
         scrollToCenter(driver, serviceRadio, actions);
@@ -58,28 +59,25 @@ public class Main {
         serviceRadio.click();
         WebElement phoneNumberInput = driver.findElement(By.xpath("//input[@id='connection-phone']"));
         phoneNumberInput.sendKeys("297777777");
-        WebElement sumOfPaymentInput = driver.findElement(By.xpath("///input[@id='connection-sum']"));
+        WebElement sumOfPaymentInput = driver.findElement(By.xpath("//input[@id='connection-sum']"));
         sumOfPaymentInput.sendKeys("10");
         WebElement continueButton = driver.findElement(By.xpath("//form[@id='pay-connection']//button[contains(@class,'button button__default')]"));
         scrollToCenter(driver, continueButton, actions);
         continueButton.click();
         System.out.println("Click!");
-        System.out.println("Текущая страница: " + driver.getCurrentUrl());
-
+        WebElement checkSumOfPaymentInput = driver.findElement(By.xpath("//p[contains(class,'header__payment-amount')]"));
+        System.out.println("Введенная сумма: 10. Отображаемая в шапке сумма: " + checkSumOfPaymentInput.getText());
+        WebElement payButton = driver.findElement(By.xpath("//form[@id='pay-connection']//button[contains(@class, 'colored disabled ng-star-inserted')]"));
+        System.out.println("Введенная сумма: 10. Отображаемая на кнопке сумма: " + payButton.getText());
         WebElement paymentLogos = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='pay__partners']")));
         scrollToCenter(driver, paymentLogos, actions);
-
         if (paymentLogos.isDisplayed()) {
             System.out.println("Логотипы платежных систем отображены");
         } else {
             System.out.println("Логотипы платежных систем не отображены");
         }
-
-
-        driver.quit();*/
+        driver.quit();
     }
-
-
     public static void scrollToCenter(WebDriver driver, WebElement element, Actions actions) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'center'});", element);
